a60b71cde28e8e057ccdd0993b62e3d2dd81c735
Shirley Cohen <shilychanxd@hotmail.com>
2025-07-21 11:53:19 +0300

    formta test

--- frontend/src/pages/AvatarShPage.tsx
+++ frontend/src/pages/AvatarShPage.tsx
@@ -3,7 +3,9 @@ import { AvatarSh } from "../features";
 export const AvatarShPage = () => {
   return (
     <div>
-      <h1>Avatar Sh Page</h1>
+      <h1>Avatar Sh Page
+        
+      </h1>
       <AvatarSh />
     </div>
   );
