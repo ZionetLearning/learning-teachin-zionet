c168a0ddc97f4b51e3de921a544026e1ebbf18d3
Shirley Cohen <shilychanxd@hotmail.com>
2025-07-21 12:29:52 +0300

    uninstalled husky

--- frontend/src/pages/AvatarShPage.tsx
+++ frontend/src/pages/AvatarShPage.tsx
@@ -3,7 +3,7 @@ import { AvatarSh } from "../features";
 export const AvatarShPage = () => {
   return (
     <div>
-      <h1>Avatar Sh Page d</h1>
+      <h1>Avatar Sh Page</h1>
       <AvatarSh />
     </div>
   );
