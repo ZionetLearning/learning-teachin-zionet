2210e91c8a0adf8c67a75986f414ad4cb43581f6
snir1551 <snir1552@gmail.com>
2025-07-21 10:48:30 +0300

    Azure Functions with Triggers Timer and Http and sent with rabbitmq

---
+++ devops/AzureFunctions/Solution1/Accessor/Models/MessageDocument.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AccessorService.Models;
+
+public class MessageDocument
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("body")]
+    public string Body { get; set; }
+
+    [BsonElement("timestamp")]
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+}
