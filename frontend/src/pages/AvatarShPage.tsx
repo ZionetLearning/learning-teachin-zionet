6040b7b189fa8af6ab28472e1333bf7ee77b61b8
Shirley Cohen <shilychanxd@hotmail.com>
2025-07-21 12:10:09 +0300

    moved to root of the project to make it work

--- frontend/src/pages/AvatarShPage.tsx
+++ frontend/src/pages/AvatarShPage.tsx
@@ -3,8 +3,10 @@ import { AvatarSh } from "../features";
 export const AvatarShPage = () => {
   return (
     <div>
-      <h1>Avatar Sh Page
-         
+      <h1>Avatar Sh Page d
+
+
+        
       </h1>
       <AvatarSh />
     </div>
