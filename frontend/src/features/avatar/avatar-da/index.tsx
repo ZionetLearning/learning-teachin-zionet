27ce74541255b0e2b9a3be9e9e25836cffc7f221
Daniel Lippo <daniil_sizov22@icloud.com>
2025-07-22 17:57:06 +0300

    avatar with azure tts working wip

--- frontend/src/features/avatar/avatar-da/index.tsx
+++ frontend/src/features/avatar/avatar-da/index.tsx
@@ -1,13 +1,19 @@
 import { Canvas } from '@react-three/fiber';
 import { Experience } from './Experience';
+import { Leva } from 'leva';
 
 export const AvatarDa = () => {
 	return (
-		<div style={{ width: 'calc(100vw - 18rem)', height: 'calc(100vh - 4rem)' }}>
-			<Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
-				<color attach="background" args={['#000']} />
-				<Experience />
-			</Canvas>
-		</div>
+		<>
+			<Leva hidden />
+			<div
+				style={{ width: 'calc(100vw - 18rem)', height: 'calc(100vh - 4rem)' }}
+			>
+				<Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
+					<color attach="background" args={['#000']} />
+					<Experience />
+				</Canvas>
+			</div>
+		</>
 	);
 };
