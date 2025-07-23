5400dd332341bb72ac3e2913729d36c5354f4f97
Benny <Benny902@gmail.com>
2025-07-23 16:05:22 +0300

    little adjustments

--- devops/kubernetes/DemoCuest/start-local.sh
+++ devops/kubernetes/DemoCuest/start-local.sh
@@ -26,7 +26,26 @@ fi
 echo "Initializing Dapr on Kubernetes..."
 dapr init -k
 
-./wait-for-dapr.sh
+# === WAIT FOR DAPR-SYSTEM READY  ===
+for i in {1..60}; do
+  POD=$(kubectl get pods -n dapr-system -l app=dapr-sidecar-injector -o jsonpath='{.items[0].metadata.name}' 2>/dev/null)
+  if [[ -n "$POD" ]]; then
+    READY=$(kubectl get pod -n dapr-system "$POD" -o jsonpath='{.status.containerStatuses[0].ready}' 2>/dev/null)
+    if [[ "$READY" == "true" ]]; then
+      echo "Dapr sidecar injector ($POD) is READY!"
+      break
+    else
+      echo "Waiting for Dapr sidecar injector ($POD) to be ready..."
+    fi
+  else
+    echo "Waiting for Dapr sidecar injector pod to be created..."
+  fi
+  sleep 2
+done
+if [[ "$READY" != "true" ]]; then
+  echo "Timeout waiting for Dapr sidecar injector to be ready."
+  exit 1
+fi
 
 # Step 3: Apply secrets
 echo "Applying secrets..."
