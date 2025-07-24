95ee916abc8121153cbcf27a4860c5a90350941c
YosefKahlon <yosefkahlon53@gmail.com>
2025-07-24 13:18:21 +0300

    Add Accessor.Functions.Services namespace
    Updated `ServiceBusConsumerFunction.cs` to include the `Accessor.Functions.Services` namespace. Removed the `using System.Threading.Tasks;` directive, suggesting a potential refactor away from asynchronous task handling.

--- devops/AzureFunctions/Solution1/Accessor/Functions/ServiceBusConsumerFunction.cs
+++ devops/AzureFunctions/Solution1/Accessor/Functions/ServiceBusConsumerFunction.cs
@@ -4,7 +4,6 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
-using System.Threading.Tasks;
 
 namespace Accessor.Functions.Functions;
 
