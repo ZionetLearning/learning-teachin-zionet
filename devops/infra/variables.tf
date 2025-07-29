3301a78351a3875bdf656906cb563107f5039875
YosefKahlon <yosefkahlon53@gmail.com>
2025-07-29 11:17:05 +0300

    Add descriptions to variables in variables.tf for clarity

--- devops/infra/variables.tf
+++ devops/infra/variables.tf
@@ -1,8 +1,10 @@
 #------------- General Variables -------------
 variable "resource_group_name" {
+  description = "Name of the resource group"
   type = string
 }
 variable "location" {
+  description = "Azure region"
   type = string
 }
 variable "subscription_id" {
@@ -17,29 +19,34 @@ variable "tenant_id" {
 
 #------------- AKS Variables -------------
 variable "aks_cluster_name" {
+  description = "Name of the AKS cluster"
   type = string
 }
 
 
 variable "node_count" {
+  description = "Number of nodes in the AKS cluster"
   type    = number
   default = 2
 }
 variable "vm_size" {
+  description = "Size of the VM instances in the AKS cluster"
   type    = string
   default = "Standard_B2s"
 }
 
 #------------- Service Bus Variables -------------
+variable "servicebus_namespace" {
+  description = "Globally unique Service Bus namespace name"
+  type = string
+}
 variable "servicebus_sku" {
+  description = "Service Bus namespace SKU (Basic, Standard, Premium)"
   type    = string
   default = "Standard"
 }
-
-variable "servicebus_namespace" {
-  type = string
-}
 variable "queue_names" {
+  description = "List of Service Bus queues to create"
   type = list(string)
   default = [
               # "clientcallback",
