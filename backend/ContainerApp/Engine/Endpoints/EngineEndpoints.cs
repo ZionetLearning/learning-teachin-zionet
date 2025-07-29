2137381c17e8e3d8f87d3066883d2706cca1840f
Shahar Amram <126160304+ShaharAmram1041@users.noreply.github.com>
2025-07-29 19:25:42 +0300

    Shahar/98/refactor api handlers in minimal api (#122)

--- backend/ContainerApp/Engine/Endpoints/EngineEndpoints.cs
+++ backend/ContainerApp/Engine/Endpoints/EngineEndpoints.cs
@@ -1,17 +1,30 @@
 using Engine.Constants;
 using Engine.Models;
 using Engine.Services;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Engine.Endpoints
-{
-public static class EngineEndpoints
-{
-    public static void MapEngineEndpoints(this WebApplication app)
+namespace Engine.Endpoints;
+
+    public static class EngineEndpoints
     {
-            
-        app.MapPost($"/{QueueNames.ManagerToEngine}-input", 
-            async (TaskModel task, IEngineService engineService, ILogger<EngineService> logger) =>
+        public static WebApplication MapEngineEndpoints(this WebApplication app)
         {
+
+            #region HTTP POST
+
+            app.MapPost($"/{QueueNames.ManagerToEngine}-input", ProcessTaskAsync).WithName("ProcessTask");
+
+            #endregion
+
+            return app;
+        }
+
+        private static async Task<IResult> ProcessTaskAsync(
+            [FromBody] TaskModel task,
+            [FromServices] IEngineService engineService,
+            [FromServices] ILogger<EngineService> logger)
+        {
+            logger.LogInformation("Inside {method}", nameof(ProcessTaskAsync));
             try
             {
                 logger.LogDebug("Received task from queue input: {Id} - {Name}", task.Id, task.Name);
@@ -24,9 +37,6 @@ public static class EngineEndpoints
                 logger.LogError(ex, "Failed to process task {Id}", task.Id);
                 return Results.Problem("An error occurred while processing the task.");
             }
-        });
-
-
-    }
-}
+        }
+    
 }
