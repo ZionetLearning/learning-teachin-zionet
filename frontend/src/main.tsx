14122ce389e9bb9c2ec1c7235e48e762a4e7aceb
otapiero <80044546+otapiero@users.noreply.github.com>
2025-07-30 16:21:13 +0300

    ouriel/137/create-a-react-query-provider-and-wrap-our-qpp-with-that (#150)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App.tsx";
 import { AppInsightsErrorBoundary } from "./components";
 import { appInsights } from "./appInsights";
+import { ReactQueryProvider } from "./providers";
 
 appInsights.loadAppInsights();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppInsightsErrorBoundary boundaryName="FrontendRootApp">
-      <App />
+      <ReactQueryProvider>
+        <App />
+      </ReactQueryProvider>
     </AppInsightsErrorBoundary>
-  </StrictMode>,
+  </StrictMode>
 );
