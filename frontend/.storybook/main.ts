cbdca7c9d446edddcc80406319f72041fdbbcc67
Yoni2222 <55746609+Yoni2222@users.noreply.github.com>
2025-07-30 08:19:40 +0300

    Yonatan/126/Unit-testing-Snapshot-testing-with-Vitest (#128)
    Co-authored-by: Shirley Cohen <shilychanxd@hotmail.com>

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,18 +1,16 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@storybook/addon-docs",
     "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@storybook/addon-vitest",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
 };
 export default config;
