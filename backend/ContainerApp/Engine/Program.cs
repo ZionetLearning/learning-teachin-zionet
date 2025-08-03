0dfd7ea8f274b1ef6bda79e39f6f40b6548a954c
Alexander Kulyabin <108358930+KrastyTC@users.noreply.github.com>
2025-08-03 15:36:37 +0300

    Alexandr/94/listener (#146)
    Added listener to engine, replacing dapr queue trigger

--- backend/ContainerApp/Engine/Program.cs
+++ backend/ContainerApp/Engine/Program.cs
@@ -1,6 +1,10 @@
+using Azure.Messaging.ServiceBus;
+using Engine.Constants;
 using Engine.Endpoints;
+using Engine.Messaging;
 using Engine.Models;
 using Engine.Services;
+using Microsoft.Azure.Amqp.Sasl;
 using Microsoft.Extensions.Options;
 using Microsoft.SemanticKernel;
 
@@ -44,6 +48,18 @@ builder.Services.AddSingleton(sp =>
                  .Build();
 });
 
+builder.Services.AddSingleton(_ =>
+    new ServiceBusClient(builder.Configuration["ServiceBus:ConnectionString"]));
+builder.Services.AddQueue<TaskModel, EngineQueueHandler>(
+    QueueNames.ManagerToEngine,
+    settings =>
+    {
+        settings.MaxConcurrentCalls = 5;
+        settings.PrefetchCount = 10;
+        settings.ProcessingDelayMs = 200;
+        settings.MaxRetryAttempts = 3;
+        settings.RetryDelaySeconds = 2;
+    });
 builder.Services.AddSingleton<ISystemPromptProvider, SystemPromptProvider>();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
@@ -54,7 +70,6 @@ var app = builder.Build();
 app.UseCloudEvents();
 app.MapControllers();
 app.MapSubscribeHandler();
-app.MapEngineEndpoints();
 app.MapAiEndpoints();
 
 app.Run();
