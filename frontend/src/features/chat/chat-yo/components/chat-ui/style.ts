73ddb8f3694a3361d3e4458a6dd67dc479f8b645
Yoni2222 <55746609+Yoni2222@users.noreply.github.com>
2025-08-04 12:38:20 +0300

    Yonatan/154/connect the chat with speaking avatar (#164)
    Co-authored-by: Shirley Cohen <shilychanxd@hotmail.com>

--- frontend/src/features/chat/chat-yo/components/chat-ui/style.ts
+++ frontend/src/features/chat/chat-yo/components/chat-ui/style.ts
@@ -1,17 +1,16 @@
 import { createUseStyles } from "react-jss";
 
 export const useStyles = createUseStyles({
-  chatWrapper: {
-    maxWidth: "800px",
-    maxHeight: "800px",
-    margin: "auto",
-    border: "1px solid #ccc",
-    padding: 10,
-  },
   messagesList: {
     height: "500px",
     overflowX: "auto",
     overflowY: "auto",
+    marginBottom: 10,
+  },
+  messagesListAvatar: {
+    flex: 1,
+    overflowY: "auto",
+    overflowX: "hidden",
     marginBottom: 10,
   },
   messageBox: {
@@ -25,20 +24,32 @@ export const useStyles = createUseStyles({
       textAlign: "right",
       justifyContent: "flex-end",
     },
+    "& svg": {
+      display: "none !important",
+    },
+  },
+  inputContainer: {
+    position: "relative",
+    flexShrink: 0,
+    padding: 6,
   },
   input: {
     border: "1px solid #ddd",
     borderRadius: "0%",
     paddingLeft: "6px",
   },
-
   inputAvatar: {
-    width: "100%!important",
+    width: "100% !important",
     fontSize: "18px",
     padding: "1px 6px 1px 6px",
     border: "1px solid #ccc",
     borderRadius: 5,
     boxSizing: "border-box",
+  },
+  rightButtons: {
+    display: "flex",
+    flexDirection: "row",
+    gap: "5px",
   },
   sendButton: {
     backgroundColor: "#44bbff",
