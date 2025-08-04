3c03fc2a2d21ca0cd7764b4d76714cd78895bdf7
dastraus007 <e-daniels@sensos.io>
2025-08-04 14:02:31 +0300

    fix comment

--- backend/ContainerApp/Engine/Messaging/QueueSettings.cs
+++ backend/ContainerApp/Engine/Messaging/QueueSettings.cs
@@ -3,8 +3,8 @@
 public class QueueSettings
 {
     public int MaxConcurrentCalls { get; set; } = 1;
-    public int PrefetchCount { get; set; }
-    public int ProcessingDelayMs { get; set; }
+    public int PrefetchCount { get; set; } = 0;
+    public int ProcessingDelayMs { get; set; } = 0;
     public int MaxRetryAttempts { get; set; } = 3;
     public int RetryDelaySeconds { get; set; } = 2;
 }
