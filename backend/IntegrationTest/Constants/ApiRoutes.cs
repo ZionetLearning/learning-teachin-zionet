3956d61a2bf57ec272081aee4011515d6512ce6c
natalidahary <128330422+natalidahary@users.noreply.github.com>
2025-08-05 13:20:51 +0300

    continue test integration (#196)

---
+++ backend/IntegrationTest/Constants/ApiRoutes.cs
@@ -0,0 +1,8 @@
+namespace IntegrationTests.Constants;
+
+public static class ApiRoutes
+{
+    public const string Task = "/task";
+    public static string TaskById(int id) => $"/task/{id}";
+    public static string UpdateTaskName(int id, string name) => $"/task/{id}/{name}";
+}
