4989b8a262f523420d4282a5a9e71590d0151e6c
snir1551 <snir1552@gmail.com>
2025-08-06 12:38:12 +0300

    refactor: streamline ingress controller setup and remove unused external IP checks

--- devops/infra/setup-ingress-controller.sh
+++ devops/infra/setup-ingress-controller.sh
@@ -4,17 +4,19 @@ set -e
 NAMESPACE="devops-ingress-nginx"
 RELEASE_NAME="ingress-nginx"
 STATIC_IP_NAME="ingress-controller-ip"
-RESOURCE_GROUP="MC_dev-zionet-learning-2025-ingress_aks-cluster-dev_westeurope"
+MC_RG="MC_dev-zionet-learning-2025-ingress_aks-cluster-dev_westeurope"
 LOCATION="westeurope"
+DNS_LABEL="teaching-zionet"
+
 
 echo "0. Uninstalling existing ingress-nginx Helm release (if present)..."
 helm uninstall "$RELEASE_NAME" -n "$NAMESPACE" || true
 
-echo "1.1 Ensure Azure public IP exists..."
-if ! az network public-ip show --resource-group "$RESOURCE_GROUP" --name "$STATIC_IP_NAME" &> /dev/null; then
-  echo "Azure Public IP '$STATIC_IP_NAME' not found in '$RESOURCE_GROUP'. Creating..."
+echo "1 Ensure Azure public IP exists..."
+if ! az network public-ip show --resource-group "$MC_RG" --name "$STATIC_IP_NAME" &> /dev/null; then
+  echo "Azure Public IP '$STATIC_IP_NAME' not found in '$MC_RG'. Creating..."
   az network public-ip create \
-    --resource-group "$RESOURCE_GROUP" \
+    --resource-group "$MC_RG" \
     --name "$STATIC_IP_NAME" \
     --sku Standard \
     --allocation-method static \
@@ -23,14 +25,22 @@ else
   echo "Azure Public IP '$STATIC_IP_NAME' already exists."
 fi
 
-echo "1. Adding ingress-nginx Helm repo..."
+echo "1.1 Adding public DNS label to your static IP..."
+az network public-ip update \
+  --resource-group "$MC_RG" \
+  --name "$STATIC_IP_NAME" \
+  --dns-name "$DNS_LABEL"
+
+
+
+echo "2. Adding ingress-nginx Helm repo..."
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx || true
 helm repo update
 
-echo "2. Creating namespace $NAMESPACE (if not exists)..."
+echo "3. Creating namespace $NAMESPACE (if not exists)..."
 kubectl get ns "$NAMESPACE" >/dev/null 2>&1 || kubectl create ns "$NAMESPACE"
 
-echo "3. Installing ingress-nginx Helm chart..."
+echo "4. Installing ingress-nginx Helm chart..."
 helm upgrade --install "$RELEASE_NAME" ingress-nginx/ingress-nginx \
   --namespace "$NAMESPACE" \
   --set controller.replicaCount=1 \
@@ -47,3 +57,6 @@ echo "✅ Ingress Controller installed."
 echo "Getting Ingress Controller External IP..."
 EXTERNAL_IP=$(kubectl get svc ingress-nginx-controller -n $NAMESPACE -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 echo "External IP: $EXTERNAL_IP"
+
+echo "✅ DNS label added. Your public address is:"
+echo "    https://${DNS_LABEL}.${LOCATION}.cloudapp.azure.com"
