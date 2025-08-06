a68d6a5c6eae0d71f583fbc2cf75951eb80cb7d5
Benny <73943596+Benny902@users.noreply.github.com>
2025-08-06 12:04:51 +0300

    Benny/add redis to cloud (#216)
    Co-authored-by: Avichai <avichai.shchori@gmail.com>

---
+++ devops/terraform/modules/redis/outputs.tf
@@ -0,0 +1,12 @@
+output "hostname" {
+  value = azurerm_redis_cache.this.hostname
+}
+
+output "port" {
+  value = azurerm_redis_cache.this.port
+}
+
+output "primary_access_key" {
+  value     = azurerm_redis_cache.this.primary_access_key
+  sensitive = true
+}
