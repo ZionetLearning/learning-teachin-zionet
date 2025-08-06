9ff62aeb8547f86043a590240f553f6225500bb7
Yonatan Ben Avraham <yoniben222@gmail.com>
2025-08-06 20:53:51 +0300

    changed typo + removed comments

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -1,20 +1,3 @@
-// import i18n from 'i18next';
-// import { initReactI18next } from 'react-i18next';
-// import LanguageDetector from 'i18next-browser-languagedetector';
-// import { resources } from './locale';      // the { en: {translation}, he: … } object
-
-// i18n
-//   .use(LanguageDetector)
-//   .use(initReactI18next)
-//   .init({
-//     resources,
-//     fallbackLng: 'en',
-//     //lng: 'en',
-//     defaultNS: 'translation',
-//     interpolation: { escapeValue: false },
-
-//   });
-
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
