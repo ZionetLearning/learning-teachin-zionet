532769926926ad33e8d725ba5b9d7b50293ba5cc
Shirley Cohen <shilychanxd@hotmail.com>
2025-08-06 08:46:03 +0300

    added some commentes

--- frontend/src/features/earthquake-map/api.ts
+++ frontend/src/features/earthquake-map/api.ts
@@ -1,9 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { Earthquake } from "./types";
 
+// USGS => United States Geological Survey, Earthquake Open API endpoint
 const buildEarthquakeUrl = (hoursAgo: number) => {
   const end = new Date().toISOString();
+  // hoursAgo * 60 * 60 * 1000 => calculating the hours in miliseconds
   const start = new Date(Date.now() - hoursAgo * 60 * 60 * 1000).toISOString();
+  console.log(`Fetching earthquakes from ${start} to ${end}`);
   return `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${start}&endtime=${end}&minmagnitude=3`;
 };
 
