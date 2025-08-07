16d0b022945e2611ed4d07b3fdc36d0df52aa555
KrastyTC <gameplese@gmail.com>
2025-08-07 17:31:15 +0300

    longrun-example
    Still in testing mode

--- backend/ContainerApp/Engine/Endpoints/EngineQueueHandler.cs
+++ backend/ContainerApp/Engine/Endpoints/EngineQueueHandler.cs
@@ -45,11 +45,56 @@ public class EngineQueueHandler : IQueueHandler<Message>
         await _engine.ProcessTaskAsync(payload, cancellationToken);
         _logger.LogInformation("Task {Id} processed", payload.Id);
     }
-
-    private Task HandleTestLongTaskAsync(Message message, Func<Task> renewLock, CancellationToken cancellationToken)
+    private async Task HandleTestLongTaskAsync(Message message, Func<Task> renewLock, CancellationToken cancellationToken)
     {
-        _logger.LogInformation("TestLongTask received — not implemented yet.");
-        return Task.CompletedTask;
+        var payload = message.Payload.Deserialize<TaskModel>();
+        if (payload is null)
+        {
+            _logger.LogWarning("Invalid payload for CreateTask");
+            return;
+        }
+
+        using var renewalCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+        var renewalTask = Task.Run(async () =>
+        {
+            try
+            {
+                while (!renewalCts.Token.IsCancellationRequested)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(20), renewalCts.Token);
+                    _logger.LogDebug("Renewing lock at {Now}", DateTime.UtcNow);
+                    await renewLock();
+                    _logger.LogDebug("Lock renewed at {Now}", DateTime.UtcNow);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // expected
+            }
+        }, renewalCts.Token);
+
+        try
+        {
+            _logger.LogInformation("Inside handler");
+
+            await Task.Delay(TimeSpan.FromSeconds(80), cancellationToken); // симуляция долгой работы
+            await _engine.ProcessTaskAsync(payload, cancellationToken);    // бизнес-логика
+        }
+        finally
+        {
+            await renewalCts.CancelAsync();
+            await renewalTask;
+        }
+        //That is simple example of how to use renewLock to prevent exciding message lock time 
+        //_logger.LogInformation("Inside handler");
+        //await renewLock();
+        //await Task.Delay(TimeSpan.FromSeconds(50), cancellationToken);
+        //_logger.LogInformation("After first pause");
+        //await renewLock();
+        //await Task.Delay(TimeSpan.FromSeconds(30), cancellationToken);
+        //_logger.LogInformation("Processing task {Id}", payload.Id);
+        //await _engine.ProcessTaskAsync(payload, cancellationToken);
+        //_logger.LogInformation("Task {Id} processed", payload.Id);
     }
 }
 
