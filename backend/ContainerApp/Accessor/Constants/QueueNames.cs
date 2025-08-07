179870e50aab5189629a243d9986dec521886453
dastraus007 <60569756+dastraus007@users.noreply.github.com>
2025-08-07 07:54:42 +0300

    Use queue listener in accessor
    Use queue listener in accessor

--- backend/ContainerApp/Accessor/Constants/QueueNames.cs
+++ backend/ContainerApp/Accessor/Constants/QueueNames.cs
@@ -3,6 +3,6 @@ namespace Accessor.Constants;
 public static class QueueNames
 {
     public const string EngineToAccessor = "engine-to-accessor";
-    public const string TaskUpdateInput = "taskupdate-input";
+    public const string TaskUpdateInput = "taskupdate";
 
 }
---
+++ backend/ContainerApp/Accessor/Messaging/QueueSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Accessor.Messaging;
+
+public class QueueSettings
+{
+    public int MaxConcurrentCalls { get; set; } = 1;
+    public int PrefetchCount { get; set; } = 0;
+    public int ProcessingDelayMs { get; set; } = 0;
+    public int MaxRetryAttempts { get; set; } = 3;
+    public int RetryDelaySeconds { get; set; } = 2;
+}
