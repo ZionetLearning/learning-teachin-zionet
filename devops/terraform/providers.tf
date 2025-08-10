374fdf31f65d1b02394c113bdab91c43e533ae34
YosefKahlon <yosefkahlon53@gmail.com>
2025-08-10 18:00:06 +0300

    Comment out Helm provider configuration for future reference

--- devops/terraform/providers.tf
+++ devops/terraform/providers.tf
@@ -16,10 +16,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.23"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.11"
-    }
+    # helm = {
+    #   source  = "hashicorp/helm"
+    #   version = "~> 2.11"
+    # }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = "~> 1.14"
