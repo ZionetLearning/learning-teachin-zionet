55e247965b782ff0d41fca0cdf98277067f8d7ff
YosefKahlon <yosefkahlon53@gmail.com>
2025-08-10 18:16:35 +0300

    Rename AKS resource from 'shared' to 'main' for clarity

--- devops/terraform/modules/aks/main.tf
+++ devops/terraform/modules/aks/main.tf
@@ -1,5 +1,5 @@
 
-resource "azurerm_kubernetes_cluster" "shared" {
+resource "azurerm_kubernetes_cluster" "main" {
   name                = var.cluster_name
   location            = var.location
   resource_group_name = var.resource_group_name
