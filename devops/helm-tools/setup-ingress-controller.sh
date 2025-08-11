393839c8f9dcf697d6798887d4406f1b4e1ac0f6
snir1551 <snir1552@gmail.com>
2025-08-11 13:10:08 +0300

    Update DNS label for ingress controller setup

--- devops/helm-tools/setup-ingress-controller.sh
+++ devops/helm-tools/setup-ingress-controller.sh
@@ -6,7 +6,7 @@ RELEASE_NAME="ingress-nginx"
 STATIC_IP_NAME="ingress-controller-ip"
 MC_RG="MC_dev-zionet-learning-2025-prom_aks-cluster-dev_westeurope"
 LOCATION="westeurope"
-DNS_LABEL="teachin-zionet"
+DNS_LABEL="teachin-zionet-prom"
 
 
 echo "0. Uninstalling existing ingress-nginx Helm release (if present)..."
