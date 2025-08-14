8d67a833483e68f34d2b76c88dac6b20d27ca4e0
Shirley Cohen <shilychanxd@hotmail.com>
2025-08-14 09:03:28 +0300

    removed initializeSentry from ChatOuPage

--- frontend/src/pages/ChatOuPage.tsx
+++ frontend/src/pages/ChatOuPage.tsx
@@ -1,11 +1,5 @@
-import { useEffect } from "react";
 import { ChatOu } from "../features";
-import { initializeSentry } from "../services";
 
 export const ChatOuPage = () => {
-  useEffect(() => {
-    initializeSentry();
-  }, []);
-
   return <ChatOu />;
 };
