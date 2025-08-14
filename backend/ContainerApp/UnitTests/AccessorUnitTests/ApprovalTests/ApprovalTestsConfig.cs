270eba6ad28902d5c6793efe70926d212c8614d1
dastraus007 <e-daniels@sensos.io>
2025-08-14 15:47:08 +0300

    add 2 approval tests

---
+++ backend/ContainerApp/UnitTests/AccessorUnitTests/ApprovalTests/ApprovalTestsConfig.cs
@@ -0,0 +1,9 @@
+﻿// Put this file in the root of the AccessorUnitTests project (same folder as the .csproj)
+using ApprovalTests.Namers;
+using ApprovalTests.Reporters;
+
+// Use a diff tool reporter (works locally; harmless in CI if no diff tool is present)
+[assembly: UseReporter(typeof(DiffReporter))]
+
+// Store snapshots under a dedicated folder in the test project
+[assembly: UseApprovalSubdirectory("Approvals")]
