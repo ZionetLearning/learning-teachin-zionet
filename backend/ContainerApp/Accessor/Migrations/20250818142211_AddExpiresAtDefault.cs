c1748fe3973c297a3044036960aa6eefc33cdb0f
Shahar Amram <126160304+ShaharAmram1041@users.noreply.github.com>
2025-08-19 12:32:57 +0300

    Shahar/319/implement refresh sessions api methods in accessor service (#371)

---
+++ backend/ContainerApp/Accessor/Migrations/20250818142211_AddExpiresAtDefault.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accessor.Migrations;
+
+/// <inheritdoc />
+public partial class AddExpiresAtDefault : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<DateTimeOffset>(
+            name: "expires_at",
+            table: "refreshSessions",
+            type: "timestamptz",
+            nullable: false,
+            defaultValueSql: "NOW() + INTERVAL '60 days'",
+            oldClrType: typeof(DateTimeOffset),
+            oldType: "timestamptz");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<DateTimeOffset>(
+            name: "expires_at",
+            table: "refreshSessions",
+            type: "timestamptz",
+            nullable: false,
+            oldClrType: typeof(DateTimeOffset),
+            oldType: "timestamptz",
+            oldDefaultValueSql: "NOW() + INTERVAL '60 days'");
+    }
+}
