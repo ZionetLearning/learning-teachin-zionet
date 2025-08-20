9659604ee33c5c06bf7986086ba72b2f82144b45
Shirley Cohen <shilychanxd@hotmail.com>
2025-08-20 10:59:14 +0300

    removed Strict Mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import "./i18n";
 import { createRoot } from "react-dom/client";
 import * as Sentry from "@sentry/react";
@@ -15,20 +14,19 @@ import App from "./App.tsx";
 initializeSentry();
 
 // const HUB_URL = "http://localhost:5280/notificationHub";
-const HUB_URL = "https://teachin.westeurope.cloudapp.azure.com/api/dev/notificationHub";
+const HUB_URL =
+  "https://teachin.westeurope.cloudapp.azure.com/api/dev/notificationHub";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <I18nTranslateProvider>
-      <ReactQueryProvider>
-        <AuthProvider>
-          <SignalRProvider hubUrl={HUB_URL}>
-            <Sentry.ErrorBoundary fallback={<div>Something went wrong</div>}>
-              <App />
-            </Sentry.ErrorBoundary>{" "}
-          </SignalRProvider>
-        </AuthProvider>
-      </ReactQueryProvider>
-    </I18nTranslateProvider>
-  </StrictMode>
+  <I18nTranslateProvider>
+    <ReactQueryProvider>
+      <AuthProvider>
+        <SignalRProvider hubUrl={HUB_URL}>
+          <Sentry.ErrorBoundary fallback={<div>Something went wrong</div>}>
+            <App />
+          </Sentry.ErrorBoundary>{" "}
+        </SignalRProvider>
+      </AuthProvider>
+    </ReactQueryProvider>
+  </I18nTranslateProvider>
 );
