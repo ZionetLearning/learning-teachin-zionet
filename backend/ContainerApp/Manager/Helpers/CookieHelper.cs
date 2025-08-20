a6cdc8d4de9f0fd9e4fa5385fab10ef6e5a6cba3
Shahar <shaharamr1041@gmail.com>
2025-08-20 13:30:34 +0300

    Some comments for the future

--- backend/ContainerApp/Manager/Helpers/CookieHelper.cs
+++ backend/ContainerApp/Manager/Helpers/CookieHelper.cs
@@ -10,6 +10,7 @@ public static class CookieHelper
         {
             HttpOnly = true, // JavaScript can’t access the cookie (mitigates XSS). 
             Secure = true, // Sent only over HTTPS
+            // In the future when have domain or frontend, consider using SameSiteMode.Lax for better CSRF protection
             SameSite = SameSiteMode.None, // Allows the cookie in cross-site requests
             Path = AuthSettings.CookiePath, //Only sent to /api/auth, not the entire domain.
             Expires = DateTimeOffset.UtcNow.AddDays(AuthSettings.RefreshTokenExpiryDays) // Expires in 7 days
@@ -21,6 +22,7 @@ public static class CookieHelper
         {
             HttpOnly = false, // Must be accessible to JS
             Secure = true,
+            // In the future when have domain or frontend, consider using SameSiteMode.Lax for better CSRF protection
             SameSite = SameSiteMode.None,
             Path = AuthSettings.CookiePath,
             Expires = DateTimeOffset.UtcNow.AddMinutes(AuthSettings.CsrfTokenExpiryMinutes) // Short-lived, 30 minutes
@@ -34,6 +36,7 @@ public static class CookieHelper
         {
             HttpOnly = true,
             Secure = true,
+            // In the future when have domain or frontend, consider using SameSiteMode.Lax for better CSRF protection
             SameSite = SameSiteMode.None,
             Path = AuthSettings.CookiePath
         });
@@ -43,6 +46,7 @@ public static class CookieHelper
         {
             HttpOnly = false,
             Secure = true,
+            // In the future when have domain or frontend, consider using SameSiteMode.Lax for better CSRF protection
             SameSite = SameSiteMode.None,
             Path = AuthSettings.CookiePath
         });
