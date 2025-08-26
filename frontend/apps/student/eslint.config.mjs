21d534068827863191ad21911c78f02e3b249a80
ShirleyCohenTN <70380850+ShirleyCohenTN@users.noreply.github.com>
2025-08-26 09:28:08 +0300

    Shirley/429/nx migration (#459)
    DevOps team need to update their workflow with the new frontend workspace (3 apps)

---
+++ frontend/apps/student/eslint.config.mjs
@@ -0,0 +1,51 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
+import storybook from "eslint-plugin-storybook";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default tseslint.config([
+  globalIgnores([
+    "dist",
+    "build",
+    "coverage",
+    "storybook-static",
+    "node_modules",
+    "cypress",
+  ]),
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: [
+          "./tsconfig.json",
+          "./tsconfig.app.json",
+          "./tsconfig.node.json",
+        ],
+        // IMPORTANT: anchor the parser to this folder
+        tsconfigRootDir: __dirname,
+        ecmaVersion: 2020,
+        sourceType: "module",
+      },
+      globals: globals.browser,
+    },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+    ],
+  },
+
+  {
+    files: ["src/**/*.stories.@(ts|tsx)"],
+    ...storybook.configs["flat/recommended"][0], // apply their first block
+  },
+]);
