67d9f6550fc193cb0e18949c392de8fc5f4749ec
ShirleyCohenTN <70380850+ShirleyCohenTN@users.noreply.github.com>
2025-08-26 10:55:50 +0300

    Shirley/461/add missing scripts to package json (#464)

--- frontend/apps/student/cypress/support/commands.ts
+++ frontend/apps/student/cypress/support/commands.ts
@@ -5,7 +5,7 @@ export {};
 Cypress.Commands.add(
   "login",
   (email: string = "admin@admin.com", password: string = "admin123") => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("http://localhost:4000/");
     // If already authenticated skip
     cy.get("body").then(($b) => {
       if ($b.find('[data-testid="ps-sidebar-container-test-id"]').length) {
