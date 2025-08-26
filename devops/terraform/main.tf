51ab3b2f63b08e356ccf1fd8d3be84f7f2a4440b
Avichai <avichai.shchori@gmail.com>
2025-08-26 17:40:05 +0300

    fix(log-analytics): update daily quota and clean up Application Insights resource

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -133,19 +133,7 @@ resource "azurerm_log_analytics_workspace" "main" {
   resource_group_name = azurerm_resource_group.main.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
-
-  tags = {
-    Environment = var.environment_name
-  }
-}
-
-# Application Insights
-resource "azurerm_application_insights" "main" {
-  name                = "${var.environment_name}-appinsights"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  application_type    = "web"
-  workspace_id        = azurerm_log_analytics_workspace.main.id
+  daily_quota_gb = 1
 
   tags = {
     Environment = var.environment_name
