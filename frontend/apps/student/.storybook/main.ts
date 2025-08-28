810e1bb5de28230f999d668ab5d4ef5063724e38
Shirley Cohen <shilychanxd@hotmail.com>
2025-08-28 08:46:50 +0300

    removed "@storybook/addon-a11y",

--- frontend/apps/student/.storybook/main.ts
+++ frontend/apps/student/.storybook/main.ts
@@ -1,15 +1,21 @@
-import type { StorybookConfig } from '@storybook/react-vite';
-import * as path from 'node:path';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import type { StorybookConfig } from "@storybook/react-vite";
+import * as path from "node:path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: StorybookConfig = {
-  framework: { name: '@storybook/react-vite', options: {} },
-  stories: ['../src/**/*.stories.@(ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-interactions'],
+  framework: { name: "@storybook/react-vite", options: {} },
+  stories: ["../src/**/*.stories.@(ts|tsx|mdx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+    "@storybook/addon-interactions",
+  ],
   viteFinal: async (config) => {
     config.plugins = [
       ...(config.plugins || []),
-      tsconfigPaths({ projects: [path.resolve(__dirname, '../../../tsconfig.base.json')] }),
+      tsconfigPaths({
+        projects: [path.resolve(__dirname, "../../../tsconfig.base.json")],
+      }),
     ];
     return config;
   },
--- frontend/libs/ui/components/.storybook/main.ts
+++ frontend/libs/ui/components/.storybook/main.ts
@@ -1,15 +1,15 @@
-import type { StorybookConfig } from '@storybook/react-vite';
-import * as path from 'node:path';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import type { StorybookConfig } from "@storybook/react-vite";
+import * as path from "node:path";
+import tsconfigPaths from "vite-tsconfig-paths";
 const config: StorybookConfig = {
-  framework: { name: '@storybook/react-vite', options: {} },
-  stories: ['../src/**/*.stories.@(ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials','@storybook/addon-a11y','@storybook/addon-interactions'],
+  framework: { name: "@storybook/react-vite", options: {} },
+  stories: ["../src/**/*.stories.@(ts|tsx|mdx)"],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
   viteFinal: async (config) => {
     config.plugins = [
       ...(config.plugins || []),
       tsconfigPaths({
-        projects: [path.resolve(__dirname, '../../../../tsconfig.base.json')]
+        projects: [path.resolve(__dirname, "../../../../tsconfig.base.json")],
       }),
     ];
     return config;
