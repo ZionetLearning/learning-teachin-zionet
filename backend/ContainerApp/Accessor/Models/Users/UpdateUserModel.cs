9a33b09791eaea06d4e8f6f1748bc0d955beed51
Shahar <shaharamr1041@gmail.com>
2025-08-31 11:51:32 +0300

    Add FirstName and LastName to the 'Users' table

--- backend/ContainerApp/Accessor/Models/Users/UpdateUserModel.cs
+++ backend/ContainerApp/Accessor/Models/Users/UpdateUserModel.cs
@@ -3,5 +3,7 @@ namespace Accessor.Models.Users;
 
 public class UpdateUserModel
 {
-    public required string Email { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
 }
--- backend/ContainerApp/Manager/Models/Users/UserModel.cs
+++ backend/ContainerApp/Manager/Models/Users/UserModel.cs
@@ -4,6 +4,8 @@ namespace Manager.Models.Users;
 public class UserModel
 {
     public Guid UserId { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
     public required string Email { get; set; }
     public required string Password { get; set; }
 }
