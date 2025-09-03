7e067207e0a1ec3c04d1dad1c88436dc925a4dff
otapiero <oht862@gmail.com>
2025-09-03 09:34:58 +0300

    Add ManagerSessionQueueHandler and update DotQueue version
    Introduces the `ManagerSessionQueueHandler` class in the `Manager.Endpoints` namespace, implementing the `IQueueHandler<Message>` interface. The class includes a logger and an empty dictionary for message handlers, with a `HandleAsync` method to process messages and handle errors. Updates the `DotQueue` package version from `1.0.1` to `1.0.3` in `Manager.csproj`.

---
+++ backend/ContainerApp/Manager/Endpoints/ManagerSessionQueueHandler.cs
@@ -0,0 +1,32 @@
+﻿using DotQueue;
+using Manager.Models.QueueMessages;
+
+namespace Manager.Endpoints;
+
+public class ManagerSessionQueueHandler : IQueueHandler<Message>
+{
+    private readonly ILogger<ManagerQueueHandler> _logger;
+    private readonly Dictionary<MessageAction, Func<Message, Func<Task>, CancellationToken, Task>> _handlers;
+
+    public ManagerSessionQueueHandler(ILogger<ManagerQueueHandler> logger)
+    {
+        _logger = logger;
+        _handlers = new Dictionary<MessageAction, Func<Message, Func<Task>, CancellationToken, Task>>
+        {
+            //[MessageAction.SpeechToTextStream] = HandleSpeechToTextStreamAsync
+        };
+    }
+
+    public async Task HandleAsync(Message message, IReadOnlyDictionary<string, string>? metadataCallback, Func<Task> renewLock, CancellationToken cancellationToken)
+    {
+        if (_handlers.TryGetValue(message.ActionName, out var handler))
+        {
+            await handler(message, renewLock, cancellationToken);
+        }
+        else
+        {
+            _logger.LogWarning("No handler for action {Action}", message.ActionName);
+            throw new NonRetryableException($"No handler for action {message.ActionName}");
+        }
+    }
+}
