816cdc7a780c0fbed915f390e1759457a73b80db
Daniel Lippo <daniil_sizov22@icloud.com>
2025-09-07 10:47:11 +0300

    fix: teacher react query client error

--- frontend/apps/teacher/src/main.tsx
+++ frontend/apps/teacher/src/main.tsx
@@ -1,18 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { AuthProvider, initAppInsights, I18nTranslateProvider} from "@app-providers";
-import { AppRole } from "@app-providers/types";
+import {
+  AuthProvider,
+  initAppInsights,
+  I18nTranslateProvider,
+} from "@app-providers";
+import { AppRole, ReactQueryProvider } from "@app-providers";
 import App from "./App.tsx";
 
 initAppInsights("teacher");
 
 createRoot(document.getElementById("root")!).render(
   <I18nTranslateProvider>
-    <AuthProvider appRole={AppRole.teacher}>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </AuthProvider>,
-  </I18nTranslateProvider>
+    <ReactQueryProvider>
+      <AuthProvider appRole={AppRole.teacher}>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </AuthProvider>
+      ,
+    </ReactQueryProvider>
+  </I18nTranslateProvider>,
 );
