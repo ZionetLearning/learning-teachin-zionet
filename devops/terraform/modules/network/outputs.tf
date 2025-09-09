44e3542102229a5ba8a6cfaafa0939485701e6f4
YosefKahlon <yosefkahlon53@gmail.com>
2025-09-09 16:01:37 +0300

    Comment out unused subnet outputs in the network module to streamline configuration for AKS-only deployment

--- devops/terraform/modules/network/outputs.tf
+++ devops/terraform/modules/network/outputs.tf
@@ -48,71 +48,71 @@ output "aks_subnet_address_prefixes" {
   value       = azurerm_subnet.aks.address_prefixes
 }
 
-#--------------------- Database Subnet Outputs ---------------------
-output "database_subnet_id" {
-  description = "ID of the database subnet"
-  value       = azurerm_subnet.database.id
-}
+# #--------------------- Database Subnet Outputs (Commented out - using public endpoints) ---------------------
+# output "database_subnet_id" {
+#   description = "ID of the database subnet"
+#   value       = azurerm_subnet.database.id
+# }
 
-output "database_subnet_name" {
-  description = "Name of the database subnet"
-  value       = azurerm_subnet.database.name
-}
+# output "database_subnet_name" {
+#   description = "Name of the database subnet"
+#   value       = azurerm_subnet.database.name
+# }
 
-output "database_subnet_address_prefixes" {
-  description = "Address prefixes of the database subnet"
-  value       = azurerm_subnet.database.address_prefixes
-}
+# output "database_subnet_address_prefixes" {
+#   description = "Address prefixes of the database subnet"
+#   value       = azurerm_subnet.database.address_prefixes
+# }
 
-#--------------------- Integration Subnet Outputs ---------------------
-output "integration_subnet_id" {
-  description = "ID of the integration subnet"
-  value       = azurerm_subnet.integration.id
-}
+# #--------------------- Integration Subnet Outputs (Commented out - not needed yet) ---------------------
+# output "integration_subnet_id" {
+#   description = "ID of the integration subnet"
+#   value       = azurerm_subnet.integration.id
+# }
 
-output "integration_subnet_name" {
-  description = "Name of the integration subnet"
-  value       = azurerm_subnet.integration.name
-}
+# output "integration_subnet_name" {
+#   description = "Name of the integration subnet"
+#   value       = azurerm_subnet.integration.name
+# }
 
-output "integration_subnet_address_prefixes" {
-  description = "Address prefixes of the integration subnet"
-  value       = azurerm_subnet.integration.address_prefixes
-}
+# output "integration_subnet_address_prefixes" {
+#   description = "Address prefixes of the integration subnet"
+#   value       = azurerm_subnet.integration.address_prefixes
+# }
 
-#--------------------- Management Subnet Outputs ---------------------
-output "management_subnet_id" {
-  description = "ID of the management subnet"
-  value       = azurerm_subnet.management.id
-}
+# #--------------------- Management Subnet Outputs (Commented out - not needed yet) ---------------------
+# output "management_subnet_id" {
+#   description = "ID of the management subnet"
+#   value       = azurerm_subnet.management.id
+# }
 
-output "management_subnet_name" {
-  description = "Name of the management subnet"
-  value       = azurerm_subnet.management.name
-}
+# output "management_subnet_name" {
+#   description = "Name of the management subnet"
+#   value       = azurerm_subnet.management.name
+# }
 
-output "management_subnet_address_prefixes" {
-  description = "Address prefixes of the management subnet"
-  value       = azurerm_subnet.management.address_prefixes
-}
+# output "management_subnet_address_prefixes" {
+#   description = "Address prefixes of the management subnet"
+#   value       = azurerm_subnet.management.address_prefixes
+# }
 
-#--------------------- All Subnets Summary ---------------------
+#--------------------- All Subnets Summary (Updated for AKS-only) ---------------------
 output "all_subnet_ids" {
   description = "Map of all subnet IDs for easy reference"
   value = {
-    aks         = azurerm_subnet.aks.id
-    database    = azurerm_subnet.database.id
-    integration = azurerm_subnet.integration.id
-    management  = azurerm_subnet.management.id
+    aks = azurerm_subnet.aks.id
+    # database    = azurerm_subnet.database.id
+    # integration = azurerm_subnet.integration.id
+    # management  = azurerm_subnet.management.id
   }
 }
 
 output "all_subnet_names" {
   description = "Map of all subnet names for easy reference"
   value = {
-    aks         = azurerm_subnet.aks.name
-    database    = azurerm_subnet.database.name
-    integration = azurerm_subnet.integration.name
-    management  = azurerm_subnet.management.name
+    aks = azurerm_subnet.aks.name
+    # database    = azurerm_subnet.database.name
+    # integration = azurerm_subnet.integration.name
+    # management  = azurerm_subnet.management.name
   }
 }
