a61599902ad8f5a72e07a9cb4a6a490361c6ddd9
YosefKahlon <yosefkahlon53@gmail.com>
2025-09-09 17:37:12 +0300

    Comment out ingress controller setup step to streamline workflow execution

--- devops/helm-tools/dapr-control-plane.sh
+++ devops/helm-tools/dapr-control-plane.sh
@@ -4,7 +4,7 @@ set -e
 
 helm repo add dapr https://dapr.github.io/helm-charts || true
 helm repo update
-kubectl get ns dapr-system >/dev/null 2>&1 || kubectl create ns dapr-system
+kubectl get ns dapr-system >/prod/null 2>&1 || kubectl create ns dapr-system
 helm upgrade --install dapr dapr/dapr \
   --version "1.15.9" \
   --namespace "dapr-system" \
