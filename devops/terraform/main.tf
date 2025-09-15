f1e8064db7272eada3cdc59f2e7b28800c95797c
snir1551 <snir1552@gmail.com>
2025-09-15 11:20:37 +0300

    Comment out monitoring module configuration in main.tf and update redis_name in terraform.tfvars.podsstart

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -146,28 +146,28 @@ locals {
   log_analytics_workspace_id = var.environment_name == "dev" ? azurerm_log_analytics_workspace.main[0].id : null
 }
 
-module "monitoring" {
-  count  = var.environment_name == "dev" ? 1 : 0
-  source = "./modules/monitoring"
+# module "monitoring" {
+#   count  = var.environment_name == "dev" ? 1 : 0
+#   source = "./modules/monitoring"
 
-  log_analytics_workspace_id  = local.log_analytics_workspace_id
-  servicebus_namespace_id     = module.servicebus.namespace_id
-  postgres_server_id          = module.database[0].id
-  signalr_id                  = module.signalr.id
-  redis_id                    = var.use_shared_redis ? data.azurerm_redis_cache.shared[0].id : module.redis[0].id
-  frontend_static_web_app_id  = length(var.frontend_apps) > 0 ? [for f in module.frontend : f.static_web_app_id] : []
+#   log_analytics_workspace_id  = local.log_analytics_workspace_id
+#   servicebus_namespace_id     = module.servicebus.namespace_id
+#   postgres_server_id          = module.database[0].id
+#   signalr_id                  = module.signalr.id
+#   redis_id                    = var.use_shared_redis ? data.azurerm_redis_cache.shared[0].id : module.redis[0].id
+#   frontend_static_web_app_id  = length(var.frontend_apps) > 0 ? [for f in module.frontend : f.static_web_app_id] : []
 
-  frontend_application_insights_ids = length(var.frontend_apps) > 0 ? [for f in module.frontend : f.application_insights_id] : []
+#   frontend_application_insights_ids = length(var.frontend_apps) > 0 ? [for f in module.frontend : f.application_insights_id] : []
 
-    depends_on = [
-    azurerm_log_analytics_workspace.main,
-    module.servicebus,
-    module.database,
-    module.signalr,
-    module.redis,
-    module.frontend
-  ]
-}
+#     depends_on = [
+#     azurerm_log_analytics_workspace.main,
+#     module.servicebus,
+#     module.database,
+#     module.signalr,
+#     module.redis,
+#     module.frontend
+#   ]
+# }
 
 ########################################
 # 2. AKS kube-config for providers
