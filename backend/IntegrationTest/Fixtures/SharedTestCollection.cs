47a534605ce0aa839569b1cf05856dd687630e57
natalidahary <128330422+natalidahary@users.noreply.github.com>
2025-09-17 15:42:34 +0300

    adding fixture to user tests (#760)

--- backend/IntegrationTest/Fixtures/SharedTestCollection.cs
+++ backend/IntegrationTest/Fixtures/SharedTestCollection.cs
@@ -5,3 +5,8 @@ public class SharedTestCollection
     : ICollectionFixture<SharedTestFixture>,
       ICollectionFixture<SignalRTestFixture>
 { }
+
+[CollectionDefinition("Per-test user collection")]
+public class PerTestUserCollection
+    : ICollectionFixture<PerTestUserFixture>
+{ }
