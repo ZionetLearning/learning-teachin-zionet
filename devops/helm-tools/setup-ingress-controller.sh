2e2f103d157dc5c8c210437e0cf97faf8915b15c
YosefKahlon <yosefkahlon53@gmail.com>
2025-09-17 12:58:53 +0300

    Add network environment configuration and update Terraform scripts
    - Introduced new environment configuration for 'network' in terraform-apply.yaml.
    - Updated setup-ingress-controller.sh to reflect the correct DNS label.
    - Commented out unused log analytics workspace and monitoring module in main.tf.
    - Added outputs for frontend applications in outputs.tf.
    - Created values.network.yaml for Helm chart overrides.
    - Added terraform.tfvars.network for resource configuration specific to the network environment.

--- devops/helm-tools/setup-ingress-controller.sh
+++ devops/helm-tools/setup-ingress-controller.sh
@@ -6,7 +6,7 @@ RELEASE_NAME="ingress-nginx"
 STATIC_IP_NAME="ingress-controller-static-ip"
 MC_RG="MC_dev-zionet-learning-2025_aks-cluster-dev_westeurope"
 LOCATION="westeurope"
-DNS_LABEL="teachin"
+DNS_LABEL="teachinnetwork"
 
 
 echo "0. Uninstalling existing ingress-nginx Helm release (if present)..."
