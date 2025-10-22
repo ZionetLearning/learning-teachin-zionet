7a0c8667b7b707bdd230967dd1fe9aa0fb3b7902
Shahar <shaharamr1041@gmail.com>
2025-10-22 14:09:00 +0300

    Add attemptId to the response from the accessor

--- backend/ContainerApp/Accessor/Models/Games/MistakeDto.cs
+++ backend/ContainerApp/Accessor/Models/Games/MistakeDto.cs
@@ -2,6 +2,7 @@ namespace Accessor.Models.Games;
 
 public class MistakeDto
 {
+    public required Guid AttemptId { get; set; }
     public required string GameType { get; set; } = string.Empty;
     public required Difficulty Difficulty { get; set; }
     public List<string> CorrectAnswer { get; set; } = new();
