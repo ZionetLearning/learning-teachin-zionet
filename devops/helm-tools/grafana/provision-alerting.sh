71489f2f531961bbe36e3f5f87d309985371928c
Avichai98 <127767873+Avichai98@users.noreply.github.com>
2025-10-23 13:18:43 +0300

    Remove Grafana rollout and status checks from alerting provision script

--- devops/helm-tools/grafana/provision-alerting.sh
+++ devops/helm-tools/grafana/provision-alerting.sh
@@ -16,23 +16,4 @@ kubectl -n "$NAMESPACE" create configmap grafana-alerting-policy \
   --from-file=notification-policy.yaml=./provisioning/alerting/notification-policy.yaml \
   --dry-run=client -o yaml | kubectl apply -f -
 
-echo "Restarting Grafana to reload alerting files..."
-kubectl rollout restart deployment "$DEPLOYMENT_NAME" -n "$NAMESPACE"
-
-set +e
-kubectl rollout status deployment "$DEPLOYMENT_NAME" -n "$NAMESPACE" --timeout="$ROLLOUT_TIMEOUT"
-RC=$?
-set -e
-
-if [ $RC -ne 0 ]; then
-  echo "Rollout timed out. Cleaning up old terminating pods and retrying..."
-  NEW_HASH="$(kubectl -n "$NAMESPACE" get deploy "$DEPLOYMENT_NAME" -o jsonpath='{.spec.template.metadata.labels.pod-template-hash}')"
-  kubectl -n "$NAMESPACE" get pods -l app.kubernetes.io/name=grafana \
-    -o=jsonpath='{range .items[*]}{.metadata.name}{"|"}{.metadata.labels.pod-template-hash}{"|"}{.metadata.deletionTimestamp}{"\n"}{end}' \
-    | awk -F"|" -v H="$NEW_HASH" '$2!=H || $3!="" {print $1}' \
-    | xargs -r -n1 kubectl -n "$NAMESPACE" delete pod --force --grace-period=0
-
-  kubectl rollout status deployment "$DEPLOYMENT_NAME" -n "$NAMESPACE" --timeout="${ROLLOUT_TIMEOUT/180s/420s}"
-fi
-
 echo "Alerting provisioned."
