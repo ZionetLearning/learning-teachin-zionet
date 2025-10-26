0e73a8603b3579e84b89100c8ae3e997286878f7
Avichai <avichai.shchori@gmail.com>
2025-10-26 08:19:30 +0200

    Update AKS schedule times and upgrade Dapr Helm chart version to 1.16.1

--- devops/helm-tools/dapr-control-plane.sh
+++ devops/helm-tools/dapr-control-plane.sh
@@ -6,7 +6,7 @@ helm repo add dapr https://dapr.github.io/helm-charts || true
 helm repo update
 kubectl get ns dapr-system >/dev/null 2>&1 || kubectl create ns dapr-system
 helm upgrade --install dapr dapr/dapr \
-  --version "1.15.9" \
+  --version "1.16.1" \
   --namespace "dapr-system" \
   --create-namespace \
   --set global.daprComponentInitTimeout="120s" \
