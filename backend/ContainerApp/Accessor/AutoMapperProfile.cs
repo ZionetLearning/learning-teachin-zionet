ff09559675535d97f4ea8f7bcd73d7ebe53f945a
Shahar Amram <126160304+ShaharAmram1041@users.noreply.github.com>
2025-10-27 09:56:00 +0200

    Shahar/879/retry logic for FE from game attempts (#881)

--- backend/ContainerApp/Accessor/AutoMapperProfile.cs
+++ backend/ContainerApp/Accessor/AutoMapperProfile.cs
@@ -1,10 +1,12 @@
-﻿using AutoMapper;
+﻿using Accessor.Models.Games;
 using Accessor.Models.Prompts;
+using AutoMapper;
 
 internal sealed class AutoMapperProfile : Profile
 {
     public AutoMapperProfile()
     {
         CreateMap<PromptModel, PromptResponse>();
+        CreateMap<GameAttempt, SubmitAttemptResult>();
     }
 }
