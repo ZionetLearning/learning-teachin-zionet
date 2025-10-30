f856e2a7f2bdfa15b0be3bf6c8a246ece48382cc
Shirley Cohen <shilychanxd@hotmail.com>
2025-10-30 13:23:03 +0200

    dark mode for Tasks feature

--- frontend/apps/admin/src/features/tasks/style.ts
+++ frontend/apps/admin/src/features/tasks/style.ts
@@ -1,16 +1,21 @@
 import { createUseStyles } from "react-jss";
+import { useThemeColors } from "@app-providers";
 
-export const useStyles = createUseStyles({
-  root: {
-    display: "flex",
-    gap: "2rem",
-    alignItems: "stretch",
-    padding: "1.5rem",
-    background: "linear-gradient(135deg,#f7f9fc 0%, #eef2f7 100%)",
-    height: "calc(100vh - 3rem)",
-    "@media (max-width: 700px)": {
-      flexDirection: "column",
-      overflowX: "auto",
+export const useStyles = () => {
+  const color = useThemeColors();
+
+  return createUseStyles({
+    root: {
+      display: "flex",
+      gap: "2rem",
+      alignItems: "stretch",
+      padding: "1.5rem",
+      background: `linear-gradient(135deg, rgba(var(${color.primaryMainChannel}) / 0.04) 0%, rgba(var(${color.primaryMainChannel}) / 0.08) 100%)`,
+      height: "calc(100vh - 3rem)",
+      "@media (max-width: 700px)": {
+        flexDirection: "column",
+        overflowX: "auto",
+      },
     },
-  },
-});
+  })();
+};
