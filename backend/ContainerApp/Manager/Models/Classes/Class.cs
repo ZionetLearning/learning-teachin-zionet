60bc90aeb9ada82be8fc43e2720c4db213a50daf
KrastyTC <gameplese@gmail.com>
2025-10-30 13:57:10 +0200

    Remove default non-null value from Code property
    The Code property in the Class model no longer defaults to null! and is now simply nullable. This change improves clarity and consistency in property initialization.

--- backend/ContainerApp/Manager/Models/Classes/Class.cs
+++ backend/ContainerApp/Manager/Models/Classes/Class.cs
@@ -4,7 +4,7 @@ public class Class
 {
     public Guid ClassId { get; set; }
     public string Name { get; set; } = null!;
-    public string? Code { get; set; } = null!;
+    public string? Code { get; set; }
     public string? Description { get; set; }
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
