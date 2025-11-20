ceabfdae83978a96c67da7e75bda8323f344978f
otapiero <oht862@gmail.com>
2025-11-20 15:31:53 +0200

    Change Classes property to List<T> in GetMyClassesResponse
    The `Classes` property in the `GetMyClassesResponse` record was
    changed from `IReadOnlyList<ClassSummaryDto>` to `List<ClassSummaryDto>`.
    This change allows the collection to be modified after initialization,
    providing more flexibility. However, it removes the immutability
    guarantees previously enforced by `IReadOnlyList<T>`, which may
    impact thread safety and immutability considerations.

--- backend/ContainerApp/Manager/Models/Classes/Responses/GetMyClassesResponse.cs
+++ backend/ContainerApp/Manager/Models/Classes/Responses/GetMyClassesResponse.cs
@@ -1,10 +1,9 @@
 namespace Manager.Models.Classes.Responses;
 
-using System.Collections.Generic;
 /// <summary>
 /// Response model for getting user's classes
 /// </summary>
 public sealed record GetMyClassesResponse
 {
-    public required IReadOnlyList<ClassSummaryDto> Classes { get; init; }
+    public required List<ClassSummaryDto> Classes { get; init; }
 }
