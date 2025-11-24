7a9a004eb0713df4d6dde93d526b4baf851a7a3d
Daniel Lippo <daniil_sizov22@icloud.com>
2025-11-24 13:16:22 +0200

    feat: useAchievementNotifications, achievements components wip

--- frontend/apps/student/src/pages/ProfilePage.tsx
+++ frontend/apps/student/src/pages/ProfilePage.tsx
@@ -1,12 +1,28 @@
+import { Box } from "@mui/material";
 import { useAuth } from "@app-providers";
 import { Profile } from "@ui-components";
+import { useGetUserAchievements } from "../api/achievements";
+import { AchievementsSection } from "../components";
 
 export const ProfilePage = () => {
   const { user } = useAuth();
+  const { data: achievements = [], isLoading } = useGetUserAchievements(
+    user?.userId,
+  );
 
   if (!user) {
-    return <div>Loading...</div>; 
+    return <div>Loading...</div>;
   }
 
-  return <Profile user={user} />;
+  //TODO: fix bottom padding
+  return (
+    <Box>
+      <Profile user={user} />
+      {!isLoading && achievements.length > 0 && (
+        <Box sx={{ marginTop: 3 }}>
+          <AchievementsSection achievements={achievements} />
+        </Box>
+      )}
+    </Box>
+  );
 };
