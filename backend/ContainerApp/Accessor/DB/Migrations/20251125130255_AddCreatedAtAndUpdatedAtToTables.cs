102a49cb8d6d2b39e2fcc8d093a50454d337d8f9
otapiero <80044546+otapiero@users.noreply.github.com>
2025-11-26 13:40:41 +0200

    Add CreatedAt/UpdatedAt fields to entities (#1057)

---
+++ backend/ContainerApp/Accessor/DB/Migrations/20251125130255_AddCreatedAtAndUpdatedAtToTables.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accessor.DB.Migrations;
+
+/// <inheritdoc />
+public partial class AddCreatedAtAndUpdatedAtToTables : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<DateTime>(
+            name: "created_at",
+            table: "UserProgress",
+            type: "timestamp with time zone",
+            nullable: false,
+            defaultValueSql: "NOW()");
+
+        migrationBuilder.AddColumn<DateTime>(
+            name: "created_at",
+            table: "UserAchievements",
+            type: "timestamp with time zone",
+            nullable: false,
+            defaultValueSql: "NOW()");
+
+        migrationBuilder.AddColumn<DateTimeOffset>(
+            name: "UpdatedAt",
+            table: "GameAttempts",
+            type: "timestamp with time zone",
+            nullable: false,
+            defaultValueSql: "NOW()");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "created_at",
+            table: "UserProgress");
+
+        migrationBuilder.DropColumn(
+            name: "created_at",
+            table: "UserAchievements");
+
+        migrationBuilder.DropColumn(
+            name: "UpdatedAt",
+            table: "GameAttempts");
+    }
+}
