35f740072d267573ad8bec01c0a9edf5fdc2e274
snir1551 <snir1552@gmail.com>
2025-12-02 16:12:43 +0200

    Update ArgoCD configurations: modify application names, namespaces, and image updater settings

--- devops/helm-tools/argocd/setup-argocd.sh
+++ devops/helm-tools/argocd/setup-argocd.sh
@@ -70,4 +70,4 @@ helm upgrade --install argocd-image-updater argo/argocd-image-updater \
 echo "ArgoCD Image Updater installation complete!"
 echo ""
 
-kubectl apply -f ./applications/applications-template.yaml
+kubectl apply -f ./applications/applications-prod.yaml
