3e25892fce2fe35a0bfa6e5194bc5b4c02332a59
Daniel Lippo <daniil_sizov22@icloud.com>
2025-12-10 17:45:01 +0200

    minor refactor

--- frontend/libs/ui/shadcn-components/src/utils.ts
+++ frontend/libs/ui/shadcn-components/src/utils.ts
@@ -2,16 +2,17 @@ import { useEffect, useState } from "react";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+/* Merge class names with tailwind-merge to dedupe Tailwind classes */
+export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
-}
+};
 
 const MOBILE_BREAKPOINT = 768;
 
 export const useIsMobile = () => {
   const [isMobile, setIsMobile] = useState<boolean | undefined>(undefined);
 
-  useEffect(() => {
+  useEffect(function handleMatchMedia() {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
     const onChange = () => setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     mql.addEventListener("change", onChange);
@@ -21,4 +22,3 @@ export const useIsMobile = () => {
 
   return !!isMobile;
 };
-
