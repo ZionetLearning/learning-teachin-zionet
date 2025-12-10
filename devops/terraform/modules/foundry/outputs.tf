303891f83dae9bd44de47f4424dd6a6c55650b75
snir1551 <snir1552@gmail.com>
2025-12-10 15:11:51 +0200

    refactor: remove unused outputs for AI Foundry in Terraform module

--- devops/terraform/modules/foundry/outputs.tf
+++ devops/terraform/modules/foundry/outputs.tf
@@ -3,15 +3,9 @@ output "foundry_id" {
   value       = azurerm_ai_foundry.foundry.id
 }
 
-output "foundry_endpoint" {
-  description = "Endpoint URL of the AI Foundry"
-  value       = azurerm_ai_foundry.foundry.endpoint
-}
-
-output "foundry_key" {
-  description = "Primary access key of the AI Foundry"
-  value       = azurerm_ai_foundry.foundry.primary_access_key
-  sensitive   = true
+output "foundry_name" {
+  description = "Name of the AI Foundry"
+  value       = azurerm_ai_foundry.foundry.name
 }
 
 output "storage_account_id" {
